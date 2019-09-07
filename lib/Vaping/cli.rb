aba2141fb182db862c655f60afb7a851a8ee2843
Dale Terrell <tragicid@gmail.com>
2019-09-07 14:56:02 +0000

    progress

--- lib/Vaping/cli.rb
+++ lib/Vaping/cli.rb
@@ -1,2 +1,32 @@
 class Vaping::CLI
+  
+  def call 
+    puts "News in vaping"
+    show_news
+    menu
+    goodbye
+  end
+  
+  def show_news
+    puts 
+    1. "D"  
+    2. "K" 
+  end
+  
+  def menu
+    puts "Which story do you wanna read? You can exit by typing 'exit'."
+    while input != "exit"
+      input = gets.strip 
+      case input 
+      when "1"
+        puts "More info on 1..."
+      when "2"
+        puts "More info on 2..."
+      end
+    end
+  end
+  
+  def goodbye
+    puts "Thanks for reading."
+  end
 end
