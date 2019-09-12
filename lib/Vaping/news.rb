3b5cf53fb6df3dfb8e638937649971854be29648
Dale Terrell <tragicid@gmail.com>
2019-09-12 15:19:25 +0000

    mehh

--- lib/Vaping/news.rb
+++ lib/Vaping/news.rb
@@ -8,11 +8,16 @@ class Vaping::News
   
   def self.scrape_news
     news = []
-    news << self.scrape_vaping360
+#    news << self.scrape_vaping360
     news << self.scrape_ava
+  end 
     
-  def self.scrape_vaping360
-    doc = Nokogiri::HTML(open("https://vaping360.com"))
-    binding.pry
-  
+#  def self.scrape_vaping360
+#    doc = Nokogiri::HTML(open("https://vaping360.com"))
+#    binding.pry
+#  end
+
+  def self.scrape_ava
+    doc = Nokogiri::HTML(open("https://https://vaping.org/news/"))
+  end
 end
